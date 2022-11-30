1367a23aa5a069be0b8a42aab60e9e2b80bda882
raj <82250359+kamaraj12312@users.noreply.github.com>
2022-11-30 18:33:36 +0530

    first commit

---
+++ mydbcontext/Dbcontext/EndocDataContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace mydbcontext
+{
+    public class EndocDataContext :DbContext
+    {
+         public EndocDataContext() : base("name=EndocDataContext")
+        {
+            
+        }
+
+        public EndocDataContext(string ConnectionString)
+            : base(ConnectionString)
+        {
+
+        }
+        public virtual DbSet<HolidaySetup> HolidaySetups { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<HolidaySetup>().ToTable("HolidaySetup", "Tenant2");
+        }
+
+    }
+
+}
---
+++ mydbcontext/Service1.svc.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Security;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace mydbcontext
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public readonly EndocDataContext endocDataContext;
+        public string ConnectionString = ConfigurationManager.ConnectionStrings["endoc"].ConnectionString;
+
+        public Service1(EndocDataContext _endocDataContext)
+        {
+            endocDataContext = _endocDataContext;
+        }
+        public Service1()
+        {
+
+        }
+        public List<HolidaySetup> GetHolidaySetup()
+        {
+            EndocDataContext obj = new EndocDataContext(ConnectionString);
+            var prodects = (from s in obj.HolidaySetups
+                            select s).ToList();
+            return prodects;
+        }
+    }
+}
